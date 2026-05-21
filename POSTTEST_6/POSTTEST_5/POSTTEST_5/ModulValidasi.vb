6d3e7f8a5cf2aa6c3133d392a1f8f2e217098aa4
WIdyaRanyy <ranyjanoor1@gmail.com>
2026-05-21 22:08:33 +0800

    validasi

--- POSTTEST_6/POSTTEST_5/POSTTEST_5/ModulValidasi.vb
+++ POSTTEST_6/POSTTEST_5/POSTTEST_5/ModulValidasi.vb
@@ -1,24 +1,21 @@
-﻿Module ModulValidasi
+Module ModulValidasi
 
     ' 1. Mengecek apakah ada TextBox/ComboBox yang kosong
-    ' Menggunakan ParamArray agar bisa mengecek banyak form sekaligus
     Public Function FormAdaYangKosong(ByVal ParamArray controls() As Control) As Boolean
         For Each ctrl As Control In controls
             If String.IsNullOrWhiteSpace(ctrl.Text) Then
                 MsgBox("Mohon lengkapi semua data! Kolom tidak boleh ada yang kosong.", MsgBoxStyle.Exclamation, "Validasi Gagal")
                 ctrl.Focus()
-                Return True ' Mengembalikan True berarti ADA yang kosong
+                Return True
             End If
         Next
-        Return False ' Mengembalikan False berarti semua form TERISI
+        Return False
     End Function
 
-    ' 2. Membatasi input agar hanya bisa diketik Angka (Untuk textbox Harga / No. Telepon)
-    ' Cara pakai: Panggil di event KeyPress pada TextBox
+    ' 2. Membatasi input agar hanya bisa diketik Angka (Untuk textbox Harga / No. Telepon
     Public Sub HanyaAngka(ByVal e As KeyPressEventArgs)
-        ' Mengizinkan angka (Digit) dan tombol kontrol seperti Backspace
         If Not Char.IsDigit(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
-            e.Handled = True ' Membatalkan input jika bukan angka
+            e.Handled = True
         End If
     End Sub
 
